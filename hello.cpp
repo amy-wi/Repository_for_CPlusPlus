e94f338b0349aea7c0c6f5fcc99233c891285c8e
amy-wi <132487968+amy-wi@users.noreply.github.com>
2024-02-28 12:42:43 +0000

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+
+//iostream is a header file that is part of c++ standard library. Here, it porvides std::cout and std::endl 
+
+#include <iostream>
+
+//All programs required "main()", it is the function executed when program starts.
+
+//In this case, main()'s return type is int because 0 is returned when program runs correctly.
+
+int main()
+
+//Curly brackets start a "scope".
+//std is the package from which these commands come from.
+//cout means c-out, and prints output to screen.
+//endl means end line.
+
+{
+    std::cout << "Hello from C++" << std::endl;
+
+    return 0;
+}
+
+//Note, every line of code ends with a semicolon.
