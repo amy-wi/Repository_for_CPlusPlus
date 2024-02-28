e6d8969532e0d3dd64eb9a8a60606a0237213d59
amy-wi <132487968+amy-wi@users.noreply.github.com>
2024-02-28 13:52:08 +0000

    Update condition.cpp

--- condition.cpp
+++ condition.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 
+//MAIN ONLY
 int main()
 {
+    //input what integer you want to test the conditions on
     int i = 55;
-
-    if (i < 10)
+    
+    //if your input integer is less or equal to than 10, then the output will print a statement to say so
+    if (i <= 10)
     {
-        std::cout << "i is less than 10" << std::endl;
+        std::cout << "i is less than or equal to 10" << std::endl;
     }
+    //if your input integer is more than or equal to 100, then the output will print a statement to say so
     else if (i >= 100)
     {
         std::cout << "i is more than or equal to 100" << std::endl;
     }
+    //if your input integer is between 10 and 100 (non inclusive), then the output will print a statement to say so
     else
     {
         std::cout << "i is somewhere between 10 and 100" << std::endl;
